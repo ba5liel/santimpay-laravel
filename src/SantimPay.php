df8e5002055fc3b14a50e19205e0e1f02b2d81ad
Basliel Selamu <baslielselamu2018@gmail.com>
2023-10-13 11:16:01 +0300

    Add test option for sandbox

--- src/SantimPay.php
+++ src/SantimPay.php
@@ -14,20 +14,21 @@ class SantimPay
     public $private_key;
 
     public $DEFAULT_HOST = 'https://services.santimpay.com/api';
+    public $DEFAULT_TEST_HOST = 'https://testnet.santimpay.com/api';
     public const API_VERSION = '/v1/gateway';
     public static $PACKAGE_VERSION = '1.0.0';
     public $DEFAULT_TIMEOUT = 1000 * 60 * 2;
     public $checkout;
     public $directPay;
 
-    public function __construct($token, $merchant_id, $private_key)
+    public function __construct($token, $merchant_id, $private_key, $test = false)
     {
         $this->token = $token;
         $this->merchant_id = $merchant_id;
         $this->private_key = $private_key;
 
         $this->http_client = new Client([
-            'base_uri' => $this->DEFAULT_HOST,
+            'base_uri' => $test? $this->DEFAULT_TEST_HOST : $this->DEFAULT_HOST,
             'headers' => [
                 "Authorization" => "Bearer $token",
                 "Content-Type" => "application/json; charset=UTF-8",
